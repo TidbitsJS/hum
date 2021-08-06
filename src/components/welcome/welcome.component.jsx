d582fdb3fcbf50ba4765c1903e12fed155b7c542
Tidbits JS <tidbitsjs@gmail.com>
2021-08-06 22:26:39 +0530

    Welcome component

---
+++ src/components/welcome/welcome.component.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import logo from "../../assets/logo.svg";
+
+import "./welcome.styles.css";
+
+const Welcome = () => {
+  return (
+    <div className="about-hum">
+      <div className="about-hum__info-container">
+        <div className="about-hum__info-container__logo">
+          <img src={logo} alt="hum-logo" />
+        </div>
+        <div className="about-hum__info-container__text">
+          <div className="about-hum__info-container__text-heading">
+            <p className="normal__text-heading">Playing</p>
+            <p className="bold__text-heading">
+              <span>Your</span> Mood
+            </p>
+            <p className="normal__text-heading">currently</p>
+          </div>
+          <div className="about_hum__info-container__para">
+            <p>
+              A platform designed to help you with the music based on your mood.
+              Feel rhythmic to search your moody songs.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="about-hum__img-container"></div>
+    </div>
+  );
+};
+
+export default Welcome;
