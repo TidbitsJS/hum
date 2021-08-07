5608844f69d79a3d8a8731514e3c0b0b90635f1e
Tidbits JS <tidbitsjs@gmail.com>
2021-08-07 22:53:18 +0530

    horizontal navigation with arrows

--- src/pages/display/display.component.jsx
+++ src/pages/display/display.component.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
 import Genre from "../../components/genre/genre.component";
+import { AiFillLeftCircle, AiFillRightCircle } from "react-icons/ai";
 
 import "./display.styles.css";
 
@@ -37,6 +38,16 @@ const random = [
 ];
 
 const DisplayPage = ({ saveResult, loading }) => {
+  const navRef = useRef();
+
+  const handleNav = (direction) => {
+    if (direction === "left") {
+      navRef.current.scrollLeft -= 200;
+    } else {
+      navRef.current.scrollLeft += 200;
+    }
+  };
+
   return (
     <div className="hum__display-page__container">
       <div className="hum__display-page__container__genres">
@@ -45,15 +56,26 @@ const DisplayPage = ({ saveResult, loading }) => {
             Explore <span>genres</span>
           </h1>
         </div>
-        <div className="hum__display-page__container__genres-box">
-          {random.map((genre) => (
-            <Genre
-              genre={genre}
-              saveResult={saveResult}
-              loading={loading}
-              key={genre.name + genre.color}
-            />
-          ))}
+        <div className="hum__display-page__container__genre-navigation">
+          <div className="hum__display-page__container__genre-navigation__back">
+            <AiFillLeftCircle onClick={() => handleNav("left")} />
+          </div>
+          <div
+            className="hum__display-page__container__genres-box"
+            ref={navRef}
+          >
+            {random.map((genre) => (
+              <Genre
+                genre={genre}
+                saveResult={saveResult}
+                loading={loading}
+                key={genre.name + genre.color}
+              />
+            ))}
+          </div>
+          <div className="hum__display-page__container__genre-navigation__next">
+            <AiFillRightCircle onClick={() => handleNav("right")} />
+          </div>
         </div>
       </div>
     </div>
